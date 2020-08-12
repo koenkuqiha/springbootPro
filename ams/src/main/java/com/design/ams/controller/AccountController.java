bdfc89a55648a4b07bb1df5752f36f2a2c1fac83
wangfeng <wangfeng@126.com>
2020-08-12 10:52:17 +0800

    ams模块提交

---
+++ ams/src/main/java/com/design/ams/controller/AccountController.java
@@ -0,0 +1,15 @@
+package com.design.ams.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/account")
+@Controller
+public class AccountController {
+
+    @RequestMapping("/list")
+    public String acountList(){
+
+        return "/acountList";
+    }
+}
